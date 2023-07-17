1e09dd2cb353467942a3d66206dc37c6269280e8
Marcus Kim <Marcus.kim@perficient.com>
2023-07-16 21:46:16 -0700

    Added getters and setters for UserBean

--- src/main/java/com/perficient/managementservice/ManagementService/UserBean.java
+++ src/main/java/com/perficient/managementservice/ManagementService/UserBean.java
@@ -9,7 +9,55 @@ public class UserBean {
 
 		private String firstName;
 
-        private String lastName;
+        public String getFirstName() {
+			return firstName;
+		}
+
+		public void setFirstName(String firstName) {
+			this.firstName = firstName;
+		}
+
+		public String getLastName() {
+			return lastName;
+		}
+
+		public void setLastName(String lastName) {
+			this.lastName = lastName;
+		}
+
+		public String getGender() {
+			return gender;
+		}
+
+		public void setGender(String gender) {
+			this.gender = gender;
+		}
+
+		public String getEmailAddress() {
+			return emailAddress;
+		}
+
+		public void setEmailAddress(String emailAddress) {
+			this.emailAddress = emailAddress;
+		}
+
+		public String getPhoneNumbers() {
+			return phoneNumbers;
+		}
+
+		public void setPhoneNumbers(String phoneNumbers) {
+			this.phoneNumbers = phoneNumbers;
+		}
+
+		public Integer getAge() {
+			return age;
+		}
+
+		public void setAge(Integer age) {
+			this.age = age;
+		}
+
+		private String lastName;
 
         private String gender;
 
