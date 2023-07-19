8bbc239ef959ba431e0236b36df9c2c0acf156cb
BrandonSPerficient <brandon.stevens@perficient.com>
2023-07-19 16:06:58 -0500

    Updated Controller

--- src/main/java/com/perficient/managementservice/ManagementService/AppointmentBean.java
+++ src/main/java/com/perficient/managementservice/ManagementService/AppointmentBean.java
@@ -1,82 +1,27 @@
 package com.perficient.managementservice.ManagementService;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AppointmentBean {
-	
-	private String apptName;
 
-	private String apptType;
-	
+
+	private Integer aptId;
+	private Integer userId;
+	private String aptType;
+	private String aptName;
 	private String description;
+	private String startTime;
+	private String endTime;
+	private String metaData;
 	
-	private LocalDateTime startTime;
-	
-	private LocalDateTime endTime;
-	
-	private String metadata;
-	
-	public AppointmentBean() {
+
 		
-	}
-
-	public AppointmentBean(String apptName, String apptType, String description, LocalDateTime startTime,
-			LocalDateTime endTime, String metadata) {
-		super();
-		this.apptName = apptName;
-		this.apptType = apptType;
-		this.description = description;
-		this.startTime = startTime;
-		this.endTime = endTime;
-		this.metadata = metadata;
-	}
-	
-	public String getApptName() {
-		return apptName;
-	}
-
-	public void setApptName(String apptName) {
-		this.apptName = apptName;
-	}
-
-	public String getApptType() {
-		return apptType;
-	}
-
-	public void setApptType(String apptType) {
-		this.apptType = apptType;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	public LocalDateTime getStartTime() {
-		return startTime;
-	}
-
-	public void setStartTime(LocalDateTime startTime) {
-		this.startTime = startTime;
-	}
-
-	public LocalDateTime getEndTime() {
-		return endTime;
-	}
-
-	public void setEndTime(LocalDateTime endTime) {
-		this.endTime = endTime;
-	}
-
-	public String getMetadata() {
-		return metadata;
-	}
-
-	public void setMetadata(String metadata) {
-		this.metadata = metadata;
-	}
 }
--- src/main/java/com/perficient/managementservice/ManagementService/ManagementServiceController.java
+++ src/main/java/com/perficient/managementservice/ManagementService/ManagementServiceController.java
@@ -35,6 +35,8 @@ public class ManagementServiceController {
     public UserBean updateUser(@PathVariable int id, @RequestBody UserBean user) throws URISyntaxException {
         URI uri = new URI("http://localhost:8200/user/"+id);
 
+        //String uri = ("http://localhost:8200/user/"+id);
+
         // Create a HttpEntity object for the updated user
         HttpEntity<UserBean> request = new HttpEntity<>(user);
 
@@ -49,38 +51,35 @@ public class ManagementServiceController {
     public List<UserBean> getUsers() {
         return restTemplate.getForObject("http://localhost:8200/api/v1/users", List.class);
     }
-
+    //works
     @GetMapping("/appointments")
     public List<AppointmentBean> getAppointments() {
-        return restTemplate.getForObject("http://localhost:8100/api/v1/users", List.class);
+        return restTemplate.getForObject("http://localhost:8100/api/v1/appointments", List.class);
     }
-    
+    //works
     @GetMapping("/appointments/{id}")
     public AppointmentBean getAppintment(@PathVariable int id) {
-        return restTemplate.getForObject("http://localhost:8100/api/v1/user/{id}", AppointmentBean.class, id);
+        return restTemplate.getForObject("http://localhost:8100/api/v1/appointments/{aptId}", AppointmentBean.class, id);
     }
-
-    @DeleteMapping("/appointments/{id}")
-    public void deleteAppointment(@PathVariable int id) {
-        restTemplate.delete("http://localhost:8100/api/v1/user/{id}", id);
+    //error
+    @DeleteMapping("/appointments/{aptId}")
+    public void deleteAppointment(@PathVariable int aptId) {
+        restTemplate.delete("http://localhost:8100/api/v1/appointments/{aptId}}", aptId);
     }
-
+    //error
     @PostMapping("/appointments")
     public AppointmentBean createAppointment(@RequestBody AppointmentBean user) {
-        return restTemplate.postForObject("http://localhost:8200/api/v1/user", user, AppointmentBean.class);
+        return restTemplate.postForObject("http://localhost:8100/api/v1/appointments", user, AppointmentBean.class);
     }
 
-    @PutMapping("/appointment/{id}")
-    public AppointmentBean updateAppointment(@PathVariable int id, @RequestBody AppointmentBean appt) throws URISyntaxException {
-        URI uri = new URI("http://localhost:8200/user/"+id);
+    @PutMapping("/appointments/{aptId}")
+    public AppointmentBean updateAppointment(@PathVariable int aptId, @RequestBody AppointmentBean appt) throws URISyntaxException {
+        String url = "http://localhost:8100/api/v1/appointments/" + aptId;
 
-        // Create a HttpEntity object for the updated user
         HttpEntity<AppointmentBean> request = new HttpEntity<>(appt);
 
-        // Make the exchange request
-        ResponseEntity<AppointmentBean> response = restTemplate.exchange(uri, HttpMethod.PUT, request, AppointmentBean.class);
+        ResponseEntity<AppointmentBean> response = restTemplate.exchange(url, HttpMethod.PUT, request, AppointmentBean.class);
 
-        // Get the updated user from the response
         return response.getBody();
     }
 
